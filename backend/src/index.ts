2fa777660bc17d7b0a500e8eecdee2094a536c76
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-07-30 15:17:41 +0530

    Updated Cors policy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from "express";
 import dotenv from "dotenv"
+import cors from "cors";
 import { connectDB } from "./config/db";
 
 import authRoutes from './routes/authRoutes'
@@ -8,6 +9,10 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT_NUMBER
 
+app.use(cors({
+    origin:["http://localhost:5173"],
+    credentials: true,
+}))
 app.use(express.json())
 
 app.use("/api/auth",authRoutes)
