07f8092e357b779d4cc43558bcad125ab5958388
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-08-22 18:14:56 +0530

    Fix CSS

--- frontend/src/components/products/ProductDetail.tsx
+++ frontend/src/components/products/ProductDetail.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom"
 import type { ProductData } from "./ProductCard";
 import axios from "../../api/axios";
@@ -8,6 +8,9 @@ import Footer from "../Footer";
 const ProductDetail = () => {
     const {productId} = useParams<{productId: string}>();
     const [product, setProduct] = useState<ProductData | null>(null)
+    const [showPreview, setShowPreview] = useState(false);
+
+    const productSectionRef = useRef<HTMLDivElement>(null);
     
     useEffect(()=>{
         const fetchProduct = async () =>{
@@ -24,9 +27,76 @@ const ProductDetail = () => {
         fetchProduct()
     },[])
 
+    useEffect(() =>{
+        const handleScroll = () => {
+            if(!productSectionRef.current) return;
+            const rect = productSectionRef.current.getBoundingClientRect();
+
+            const threshold = 100;
+            setShowPreview(rect.top < threshold);
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        handleScroll();
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    },[])
+
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            // Adjust this threshold as needed
+            setIsScrolled(window.scrollY > 100);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
   return (
     <>
         <div className="mx-auto w-4/5 xl:w-[1040px]">
+            
+            {showPreview &&
+                <div className="fixed top-[68px] left-0 z-[101] w-full transition-all duration-300 lg:top-[86px] translate-y-0 opacity-100">
+                    <div className="w-screen bg-white shadow-[0px_4px_34.2px_0px_rgba(0,0,0,0.10)]">
+                        <div className="mx-auto flex w-4/5 items-center justify-between bg-white py-2.5 xl:w-[1040px]">
+                            <div className="relative flex gap-4">
+                                <img alt="4e44890d-d561-4972-b190-45b0324acce8.jpeg" fetchPriority="low" loading="lazy" width="100" height="100" decoding="async" data-nimg="1" className="relative overflow-hidden rounded-xl" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" srcSet="" src="https://cdn.zeptonow.com/production/tr:w-100,ar-100-100,pr-true,f-auto,q-80/cms/product_variant/4e44890d-d561-4972-b190-45b0324acce8.jpeg" style={{color: "transparent", objectFit: "contain"}}/>
+                                <div className="">
+                                    <h2 className="text-sm font-semibold leading-6 tracking-[-0.3px] text-[#262A33]">{product?.title}</h2>
+                                    <div className="mt-1.5">
+                                        <div className="flex items-center gap-2 shrink-0" data-discountpercent="89" data-getforpassprice="3900">
+                                            <p className="text-[24px] font-medium leading-5 text-[#262A33]">₹{product?.currPrice}</p>
+                                            <p className="text-[16px] font-[450] leading-[21.6px] tracking-[-0.24px] text-[#A9B0C2] line-through">₹{product?.mrp}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div aria-label="Add to Cart" className="flex h-12 justify-between overflow-hidden rounded-[10px] border border-[#EF4372] bg-[#EF4372] px-4 py-3 font-bold leading-5 tracking-[0.1px] text-white transition-all duration-300 ease-in-out w-60">
+                                    <button aria-label="Increase quantity by 1" className="w-full disabled:opacity-60">Add To Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+
+            <div className="flex flex-wrap items-center my-8 gap-2" data-testid="pdp-breadcrumbs">
+                <span className="flex shrink-0 items-center gap-2">
+                    <span><a className="text-sm font-[450] leading-[14px] text-[#5A6477]" href="/">Home</a></span>
+                    <svg fill="none" height="16" viewBox="0 0 17 16" width="17" xmlns="http://www.w3.org/2000/svg" style={{height: "15px", width: "15px", color: "rgb(90, 100, 119)"}}><path d="M7 4.5L10.5 8L7 11.5" stroke="#5A6477" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path></svg>
+                </span>
+                <span className="flex shrink-0 items-center gap-2">
+                    <span><a className="text-sm font-[450] leading-[14px] text-[#5A6477]" href="/cn/zepto-cafe/tea-coffee/cid/dfe16918-81b1-49c3-941b-acd557f7c277/scid/9517d4fc-47f8-408d-8368-6ae245ce42b5">Tea &amp; Coffee</a></span>
+                    <svg fill="none" height="16" viewBox="0 0 17 16" width="17" xmlns="http://www.w3.org/2000/svg" style={{height: "15px", width: "15px", color: "rgb(90, 100, 119)"}}><path d="M7 4.5L10.5 8L7 11.5" stroke="#5A6477" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path></svg>
+                </span>
+                <span className="text-sm font-semibold leading-[14px] text-[#101418]">{product?.title}</span>
+            </div>
             <div className="mt-8 flex flex-col gap-5 md:flex-row">
                 <div className="bottom-0 top-[118px] flex w-full flex-col gap-5 self-start overflow-y-hidden lg:sticky" id="left-carousel">
                     <div>
@@ -57,15 +127,20 @@ const ProductDetail = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="ml-[68px]">
+                    <div className="ml-[68px]" ref={productSectionRef}>
                         <div className="relative flex items-center justify-between bg-white">
-                            <div className="pointer-events-none w-0 -translate-x-8 opacity-0" style={{transform: "translateX(-10px)", width: "0%", opacity: "0"}}>
+                            <div className={`pointer-events-none transition-all duration-300 ${
+                                    isScrolled 
+                                        ? 'opacity-100 translate-x-0 w-auto' 
+                                        : 'opacity-0 -translate-x-8 w-0'
+                                }`}>
                                 <div className="flex items-center gap-2 shrink-0" data-discountpercent="87" data-getforpassprice="5700">
-                                    <p className="text-[24px] font-medium leading-5 text-[#262A33]">₹57</p>
-                                    <p className="text-[16px] font-[450] leading-[21.6px] tracking-[-0.24px] text-[#A9B0C2] line-through">₹469</p>
+                                    <p className="text-[24px] font-medium leading-5 text-[#262A33]">₹{product?.currPrice}</p>
+                                    <p className="text-[16px] font-[450] leading-[21.6px] tracking-[-0.24px] text-[#A9B0C2] line-through">₹{product?.mrp}</p>
                                 </div>
                             </div>
-                            <div aria-label="Add to Cart" className="flex h-12 justify-between overflow-hidden rounded-[10px] border border-[#EF4372] bg-[#EF4372] px-4 py-3 font-bold leading-5 tracking-[0.1px] text-white transition-all duration-300 ease-in-out w-full" style={{width: "100%"}}>
+                            <div aria-label="Add to Cart" className={`flex h-12 items-center justify-center rounded-[10px] border border-[#EF4372] bg-[#EF4372] font-bold leading-5 tracking-[0.1px] text-white transition-all duration-300 ease-in-out 
+                                                                        ${isScrolled ? 'w-32 px-3 py-2 ml-auto' : 'w-full px-4 py-3'}`} >
                                 <button aria-label="Increase quantity by 1" className="w-full disabled:opacity-60">Add To Cart</button>
                             </div>
                         </div>
