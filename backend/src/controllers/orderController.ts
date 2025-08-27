c8306d281d93a113073b632e0f45d741012e8636
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-08-27 13:44:41 +0530

    Added GET route orders

--- backend/src/controllers/orderController.ts
+++ backend/src/controllers/orderController.ts
@@ -3,6 +3,24 @@ import { Response } from "express";
 import User from "../models/User";
 import Order from "../models/Order";
 
+export const getOrders = async (req: AuthRequest, res: Response) => {
+    const userId = req.userId
+
+    try{
+        if(!userId) return res.status(404).json({message: "User ID not send"})
+
+        const user = await User.findById(userId)
+        if(!user) return res.status(404).json({message: "User not found"})
+
+        const Allorders = await Order.findById(userId).sort({ createdAt: -1 }); 
+
+        res.status(200).json({message: "Orders fetched successfully", orders: Allorders })
+    }
+    catch(error){
+        console.error("Error creating order:", error);
+        return res.status(500).json({ message: "Internal Server Error." });
+    }
+}
 
 export const createOrder = async (req: AuthRequest, res: Response) => {
     const userId = req.userId
--- backend/src/routes/orderRoutes.ts
+++ backend/src/routes/orderRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express"
-import { createOrder } from "../controllers/orderController"
+import { createOrder, getOrders } from "../controllers/orderController"
 import { authMiddleware } from "../middlewares/authMiddleware"
 
 const router = express.Router()
 
+router.get("/",authMiddleware,getOrders)
 router.post("/",authMiddleware,createOrder)
 
 export default router
